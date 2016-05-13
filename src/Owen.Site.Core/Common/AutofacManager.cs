56b5a8240f9f7d14b2e2e9c03751aaa4294fcd56
Owen-Zhang <1034992601@qq.com>
2016-05-13 22:16:16 +0800

    no message

--- src/Owen.Site.Core/Common/AutofacManager.cs
+++ src/Owen.Site.Core/Common/AutofacManager.cs
@@ -1,29 +1,62 @@
-﻿using Autofac;
+﻿using System.IO;
+using Autofac;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Web;
 namespace Owen.Site.Core.Common
 {
     public class AutofacManager
     {
+        static AutofacManager manager;
+        static IContainer container;
+
+        AutofacManager()
+        {    
+        }
+
+        public static AutofacManager Current {
+            get
+            {
+                if (manager == null)
+                    manager = new AutofacManager();
+                return manager;
+            }
+        }
+
+        public IContainer Container
+        {
+            get
+            {
+                if (container == null)
+                    Init();
+                return container;
+            }
+        }
+
         public void Init()
         {
             var assemblyStrList = new List<string>{
-                "",
-                "",
-                "",
-                ""
+                "Owen.Site.Data.Service.dll",
+                "Owen.Site.Data.Service.Imp.dll",
+                "Owen.Site.Main.Service.dll",
+                "Owen.Site.Main.Service.Imp.dll"
             };
-            /*
-            Assembly.LoadFile()
+
+            List<Assembly> assemblies = new List<Assembly>();
+            foreach (var item in assemblyStrList)
+	        {
+                assemblies.Add(
+                    Assembly.LoadFile(Path.Combine(HttpContext.Current.Server.MapPath("~"), "bin", item)));
+	        }
+            
             ContainerBuilder builder = new ContainerBuilder();
-            builder.RegisterAssemblyTypes(assemblies)
-            .Where(type => baseType.IsAssignableFrom(type) && !type.IsAbstract)
-            .AsImplementedInterfaces().InstancePerLifetimeScope();//InstancePerLifetimeScope 保证对象生命周期基于请求
-                    IContainer container = builder.Build();
-                    DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-             */
+            builder.RegisterAssemblyTypes(assemblies.ToArray())
+                    .Where(type => typeof(IDependency).IsAssignableFrom(type) && !type.IsAbstract)
+                    .AsImplementedInterfaces().InstancePerLifetimeScope();
+            container = builder.Build();
+            new ServiceProvider(container);
         }
     }
 }
--- src/Owen.Site.Services/Map/ListMapInfoService.cs
+++ src/Owen.Site.Services/Map/ListMapInfoService.cs
@@ -3,31 +3,17 @@ using Owen.Site.Core.Log;
 using Owen.Site.Model;
 using Owen.Site.Services.Common;
 using Owen.Site.Main.Service;
+using Autofac;
+using Owen.Site.Core.Common;
 
 namespace Owen.Site.Services.Map
 {
     public class ListMapInfoService : BaseService<ListMapInfoServiceRequest>
     {
-        MapDomainService doMainService;
-        public ListMapInfoService(MapDomainService doMainService)
-        {
-            this.doMainService = doMainService;
-        }
-
         public override object OnGet(ListMapInfoServiceRequest req)
         {
-            return new List<MapInfo> { 
-                new MapInfo {
-                     Address = "成都中和镇",
-                     XPoint = "23.2356",
-                     YPoint = "106.4586"
-                },
-                new MapInfo {
-                     Address = "成都双流",
-                     XPoint = "23.856",
-                     YPoint = "106.145"
-                }
-            };
+            return
+            AutofacManager.Current.Container.Resolve<MapDomainService>().GetMapList();
         }
 
         public override object OnPost(ListMapInfoServiceRequest req)
