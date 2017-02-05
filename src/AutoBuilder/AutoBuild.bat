5172fc5522c4ec5457dacc066094a45a6ddf538c
Owen-Zhang <1034992601@qq.com>
2017-02-05 21:34:29 +0800

    no message

---
+++ src/AutoBuilder/AutoBuild.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+cd /d %~dp0
+
+set outputDir=%cd%
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.sln /p:Configuration=Release /t:Clean
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.Host\Owen.Site.Host.csproj /p:Configuration=Release /t:Clean
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.Host\Owen.Site.Host.csproj /p:Configuration=Release /t:Rebuild
+
+xcopy /s ..\Owen.Site.Host\bin  %outputDir%\OutPut\
+
+@pause 
