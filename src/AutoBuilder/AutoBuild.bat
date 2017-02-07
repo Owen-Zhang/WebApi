24d511f90109d2bcfd60077d86ddbd4cea50c818
Owen-Zhang <1034992601@qq.com>
2017-02-07 21:10:52 +0800

    no message

--- src/AutoBuilder/AutoBuild.bat
+++ src/AutoBuilder/AutoBuild.bat
@@ -3,11 +3,29 @@ cd /d %~dp0
 
 set outputDir=%cd%
 
+::清除已生成的文件
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.sln /p:Configuration=Release /t:Clean
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.Host\Owen.Site.Host.csproj /p:Configuration=Release /t:Clean
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.Host\Owen.Site.Host.csproj /p:Configuration=Release /t:Rebuild
+::删除output下生成的临时文件
+rd /s /q %outputDir%\OutPut\
 
-xcopy /s ..\Owen.Site.Host\bin  %outputDir%\OutPut\
+::重新编译生成文件
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" ../Owen.Site.Host\Owen.Site.Host.csproj /p:Configuration=Release;OutDir=%outputDir%\OutPut\bin /t:Rebuild 
+
+set binPath=%outputDir%\OutPut\bin
+
+::删除bin下面的文件夹
+for /f %%s in ('dir /s /a /b %binPath%') do (
+	if exist %%s\ (rd /s /q %%s)
+)
+
+::复制数据访问相关的sql
+xcopy /s ..\Owen.Site.Data.Service.Imp\Configuration  %outputDir%\OutPut\Configuration\
+
+::复制host工程中的一些文件或者某些文件夹下面的所有文件（）
+for /f %%i in (HostMustCopyFileList.txt) do (HostMustCopyFileList.txt在里面配制)
+	if exist ..\Owen.Site.Host\%%i\ (xcopy /s ..\Owen.Site.Host\%%i %outputDir%\OutPut\%%i\) else (xcopy /s ..\Owen.Site.Host\%%i %outputDir%\OutPut\)
+)
 
 @pause 
